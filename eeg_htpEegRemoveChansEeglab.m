c693d923bd7e89027694d4655bc050d60abb9f86
Ernie Pedapati <ernest.pedapati@cchmc.org>
2022-06-17 09:20:01 -0400

    added parameters 'removechannel' (removes channels, not just mark) and 'automark' which forgoes automatic detection

--- eeg_htpEegRemoveChansEeglab.m
+++ eeg_htpEegRemoveChansEeglab.m
@@ -21,11 +21,23 @@ function [EEG, results] = eeg_htpEegRemoveChansEeglab(EEG,varargin)
 %                       default: 100
 %
 %   'threshold' - Number to utilize for threshold in automated detection/marking
-%                 of bad channels via various measures (probability, kurtosis,and spectrum) 
+%                 of bad channels via various measures (probability, kurtosis,and spectrum)
 %                 default: 5
-%   
+%
+%    'removechannel' - true/false if channels should be removed after
+%                       marking prior to next step.
+%    
+%    'automark'      - turns on and off automatic detection (default:
+%    false)
+%
 % Outputs:
 %     EEG         - Updated EEGLAB structure
+%
+% Version history:
+% 6/16/2022       EP added option 'removechannel', true/false to delete
+%           channels without interpolation before the next step.
+%                 default: false
+% 6/17/2022       EP added automark option (default: false)
 %
 %  This file is part of the Cincinnati Visual High Throughput Pipeline,
 %  please see http://github.com/cincibrainlab
@@ -35,6 +47,8 @@ function [EEG, results] = eeg_htpEegRemoveChansEeglab(EEG,varargin)
 defaultType = 'Resting';
 defaultMinimumDuration = 100;
 defaultThreshold = 5;
+defaultRemoveChannel = false;
+defaultAutoMark = false;
 
 validateType = @( type ) ischar( type ) && ismember(type, {'Resting', 'Event'});
 
@@ -45,6 +59,8 @@ addRequired(ip, 'EEG', @isstruct);
 addParameter(ip,'type',defaultType,validateType);
 addParameter(ip, 'minimumduration',defaultMinimumDuration,@isnumeric);
 addParameter(ip,'threshold',defaultThreshold,@isnumeric);
+addParameter(ip,'removechannel', defaultRemoveChannel, @islogical);
+addParameter(ip, 'automark', defaultAutoMark, @islogical);
 
 parse(ip,EEG,varargin{:});
 
@@ -54,119 +70,131 @@ timestamp = datestr(now, 'yymmddHHMMSS'); % timestamp
 functionstamp = mfilename; % function name for logging/output
 repeating = 1;
 try
-   if isfield(EEG.vhtp,'eeg_htpEegRemoveChansEeglab') && isfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'failReason')
-       EEG.vhtp.eeg_htpEegRemoveChansEeglab=rmfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'failReason');
-   end
-   original = EEG;
-   if EEG.xmax < ip.Results.minimumduration
-       EEG.vhtp.eeg_htpEegRemoveChansEeglab.completed = 0;
-       EEG.vhtp.eeg_htpEegRemoveChansEeglab.failReason = 'Data too short';
+    if isfield(EEG.vhtp,'eeg_htpEegRemoveChansEeglab') && isfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'failReason')
+        EEG.vhtp.eeg_htpEegRemoveChansEeglab=rmfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'failReason');
+    end
+    original = EEG;
+    if EEG.xmax < ip.Results.minimumduration
+        EEG.vhtp.eeg_htpEegRemoveChansEeglab.completed = 0;
+        EEG.vhtp.eeg_htpEegRemoveChansEeglab.failReason = 'Data too short';
 
-       if strcmp(ip.Results.type,'Resting')
-           f = errordlg(sprintf('\t\tYOUR DATA IS SHORTER THAN THE SET MINIMUM DURATION OF %d SECONDS\n\n\t\tYOUR FILE WILL NOT UNDERGO TRIMMING EDGES OR MARKING BAD CHANNELS',ip.Results.minimumduration),'Recording Error');
-       else
-           f = errordlg(sprintf('\t\tYOUR DATA IS SHORTER THAN THE SET MINIMUM DURATION OF %d SECONDS\n\n\t\tYOUR FILE WILL NOT UNDERGO MARKING BAD CHANNELS.',ip.Results.minimumduration),'Recording Error');
-       end
-       uiwait(f);
-       repeating=0;
-           
-   end
-   if strcmp(ip.Results.type,'Resting') 
-       if ~isfield(EEG.vhtp,'eeg_htpEegRemoveChansEeglab')
-           EEG = trim_edges(EEG,10);
-           if isfield(EEG.vhtp,'eeg_htpEegRemoveChansEeglab') && isfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'failReason')
-               f=errordlg(sprintf('\t\tYOUR DATA IS SHORTER THAN THE SET MINIMUM DURATION OF %d SECONDS\n\n\t\tYOUR FILE WILL NOT UNDERGO MARKING BAD CHANNELS.',ip.Results.minimumduration));
-               uiwait(f);
-               repeating = 0;
-           end
-       end
-   end
-   while repeating
-       EEG = original;
+        if strcmp(ip.Results.type,'Resting')
+            f = errordlg(sprintf('\t\tYOUR DATA IS SHORTER THAN THE SET MINIMUM DURATION OF %d SECONDS\n\n\t\tYOUR FILE WILL NOT UNDERGO TRIMMING EDGES OR MARKING BAD CHANNELS',ip.Results.minimumduration),'Recording Error');
+        else
+            f = errordlg(sprintf('\t\tYOUR DATA IS SHORTER THAN THE SET MINIMUM DURATION OF %d SECONDS\n\n\t\tYOUR FILE WILL NOT UNDERGO MARKING BAD CHANNELS.',ip.Results.minimumduration),'Recording Error');
+        end
+        uiwait(f);
+        repeating=0;
 
-       gui.position = [0.01 0.20 0.80 0.70];
-       EEG=autobadchannel( EEG,ip.Results.threshold );
+    end
+    if strcmp(ip.Results.type,'Resting')
+        if ~isfield(EEG.vhtp,'eeg_htpEegRemoveChansEeglab')
+            EEG = trim_edges(EEG,10);
+            if isfield(EEG.vhtp,'eeg_htpEegRemoveChansEeglab') && isfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'failReason')
+                f=errordlg(sprintf('\t\tYOUR DATA IS SHORTER THAN THE SET MINIMUM DURATION OF %d SECONDS\n\n\t\tYOUR FILE WILL NOT UNDERGO MARKING BAD CHANNELS.',ip.Results.minimumduration));
+                uiwait(f);
+                repeating = 0;
+            end
+        end
+    end
+    while repeating
+        EEG = original;
+
+        gui.position = [0.01 0.20 0.80 0.70];
+        EEG=autobadchannel( EEG,ip.Results.threshold, ip.Results.automark );
 
 
-       cdef = {'b','b'};
-       carr = repmat(cdef,1, size(EEG.data,1));
-       carr = carr(1:size(EEG.data, 1));
-       carr(EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_autobadchannel) = {'r'};
+        cdef = {'b','b'};
+        carr = repmat(cdef,1, size(EEG.data,1));
+        carr = carr(1:size(EEG.data, 1));
+        carr(EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_autobadchannel) = {'r'};
 
-       eegplot(EEG.data,'srate',EEG.srate,'winlength',10, ...
+        eegplot(EEG.data,'srate',EEG.srate,'winlength',10, ...
             'plottitle', [sprintf('Mark and Remove Bad Channels for Subject %s',regexprep(EEG.subject,'^*\.\w+$',''))], ...
             'events',EEG.event,'color',carr,'wincolor',[1 0.5 0.5], ...
             'eloc_file',EEG.chanlocs,  'butlabel', 'Close Window', 'submean', 'on', ...
             'command', 't = 1', 'position', [400 400 1024 768] ...
             );
 
-       h = findobj('tag', 'eegplottitle');
-       h.FontWeight = 'Bold'; h.FontSize = 16; h.Position = [0.5000 0.93 0];
-       proc_badchans=[];
-       chanlist = {EEG.chanlocs.labels};
+        h = findobj('tag', 'eegplottitle');
+        h.FontWeight = 'Bold'; h.FontSize = 16; h.Position = [0.5000 0.93 0];
+        proc_badchans=[];
+        chanlist = {EEG.chanlocs.labels};
 
-       if ~isfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'proc_badchans')
-           EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans =  '';
-       end
+        if ~isfield(EEG.vhtp.eeg_htpEegRemoveChansEeglab,'proc_badchans')
+            EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans =  '';
+        end
 
-       handle = gcf;
-       handle.Units = 'normalized';
-       handle.Position = gui.position;
+        handle = gcf;
+        handle.Units = 'normalized';
+        handle.Position = gui.position;
 
 
-       popup = uicontrol(handle,'Tag', 'chanselect', 'Style', 'listbox', ...
+        popup = uicontrol(handle,'Tag', 'chanselect', 'Style', 'listbox', ...
             'max',EEG.nbchan,'min',1, ...
             'String', chanlist , 'Value', EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_autobadchannel,...
             'Units', 'normalized', ...
             'Position', [.05 0.15 0.035 .70], 'BackgroundColor', [0.94 0.94 0.94]);
 
-       showBadDetail = uicontrol(handle,...
+        showBadDetail = uicontrol(handle,...
             'Tag', 'detailbutton', ...
             'Style', 'pushbutton', 'BackgroundColor', [0 1 1],...
             'Units', 'normalized', ...
             'Position', [0.7 0.08 0.10 0.05],...
             'String', 'Detail', 'Callback', @(src,event)showChanDetail(EEG));
 
-       toggleBadChannels = uicontrol(handle,...
+        toggleBadChannels = uicontrol(handle,...
             'Tag', 'savebutton', ...
             'Style', 'togglebutton', 'BackgroundColor', [0 1 0],...
             'Units', 'normalized', ...
             'Position', [0.8 0.08 0.14 0.05],...
             'String', 'Save', 'Callback', @(src,event)selBadChan(EEG));
 
-       textBadChannels = uicontrol(handle, 'Style', 'text', ...
+        textBadChannels = uicontrol(handle, 'Style', 'text', ...
             'String', 'Manual Bad Channel Rejection: no channels selected', 'Tag', 'badchantitle', ...
             'FontSize', 14,    'Units', 'normalized', 'Position', [0.1 0.89 0.3 0.03], 'HorizontalAlignment', 'left');
 
 
-       waitfor(gcf);
+        waitfor(gcf);
 
-       if length(proc_badchans) > ceil(EEG.nbchan*.05)
-           f=warndlg('YOU HAVE REMOVED MORE THAN 5% OF THE TOTAL NUMBER OF CHANNELS');
-           uiwait(f);
-       end
+        if length(proc_badchans) > ceil(EEG.nbchan*.05)
+            f=warndlg('YOU HAVE REMOVED MORE THAN 5% OF THE TOTAL NUMBER OF CHANNELS');
+            uiwait(f);
+        end
 
-       EEG.vhtp.eeg_htpEegRemoveChansEeglab.completed=1;
-       if ~isempty(EEG.vhtp.eeg_htpEegRemoveChansEeglab.('proc_badchans'))
-           EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans = sort(unique([EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans, proc_badchans]));
-       else 
-           EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans = proc_badchans;
-       end
-       answer = questdlg(sprintf('Would you like to Re-do the Marking Bad Channel Process for Subject %s?',regexprep(EEG.subject,'^*\.\w+$','')),'Channel Removal Repeat','Repeat','Continue','Continue');
-       if isempty(answer) || strcmp(answer, 'Repeat')
-           repeating = 1;
-       else
-           repeating = 0;
-       end
-   end
-   
+        EEG.vhtp.eeg_htpEegRemoveChansEeglab.completed=1;
+        if ~isempty(EEG.vhtp.eeg_htpEegRemoveChansEeglab.('proc_badchans'))
+            EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans = sort(unique([EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans, proc_badchans]));
+        else
+            EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_badchans = proc_badchans;
+        end
+        answer = questdlg(sprintf('Would you like to Re-do the Marking Bad Channel Process for Subject %s?',regexprep(EEG.subject,'^*\.\w+$','')),'Channel Removal Repeat','Repeat','Continue','Continue');
+        if isempty(answer) || strcmp(answer, 'Repeat')
+            repeating = 1;
+        else
+            repeating = 0;
+        end
+    end
+
 catch e
     throw(e)
 end
 
-
-
 EEG=eeg_checkset(EEG);
+
+% EP update 6/16/2022
+% add option to remove channels without interpolation
+% added to optimize inline ASR
+
+if ip.Results.removechannel
+    fprintf('eeg_htpEegRemoveChannelsEeglab: Remove Channels ON \n');
+    channel_labels = {EEG.chanlocs.labels};
+    nochannel_idx = channel_labels(proc_badchans);
+    EEG = pop_select( EEG, 'nochannel',  nochannel_idx);
+else
+    fprintf('eeg_htpEegRemoveChannelsEeglab: Mark Channels Only \n')
+end
+%
 
 qi_table = cell2table({EEG.filename, functionstamp, timestamp}, ...
     'VariableNames', {'eegid','scriptname','timestamp'});
@@ -177,8 +205,7 @@ else
 end
 results = EEG.vhtp.eeg_htpEegRemoveChansEeglab;
 
-function  EEG=showChanDetail(EEG)
-
+    function  EEG=showChanDetail(EEG)
 
     ui_main = findobj(gcf,'Tag', 'EEGPLOT');
     h = findobj(gcf,'Style','togglebutton');
@@ -189,16 +216,14 @@ function  EEG=showChanDetail(EEG)
     ui_selection = hlist.Value;
 
     if ~isempty(ui_selection)
-
         pop_prop( EEG, 1, ui_selection, NaN, {'freqrange' [0.5 55] });
+    end
 
     end
 
-end
+    function EEG=selBadChan(EEG)
 
-function EEG=selBadChan(EEG)
 
-    
     ui_main = findobj(gcf,'Tag', 'EEGPLOT');
     h = findobj(gcf,'Style','togglebutton');
     hlist = findobj(gcf,'Style','listbox');
@@ -221,33 +246,35 @@ function EEG=selBadChan(EEG)
         proc_badchans=[];
     end
 
-end
+    end
 
 end
 
 
 function EEG = trim_edges( EEG, time )
-    if isempty(EEG)
-        
-    else
-        EEGTMP = EEG;
-        if time * 6 > EEG.xmax
-            validtime = 0;
-            EEGTMP.vhtp.eeg_htpEegRemoveChansEeglab.completed=0;
-            EEGTMP.vhtp.eeg_htpEegRemoveChansEeglab.failReason = 'Data too short';
-        else
-            cut1 = [0 time];
-            EEGTMP = eeg_checkset(pop_select(EEGTMP, 'notime', cut1));
-            cut2 = [(EEGTMP.xmax - time) EEGTMP.xmax];
-            EEGTMP = eeg_checkset(pop_select(EEGTMP, 'notime', cut2));
-        end
+if isempty(EEG)
 
-        EEG = EEGTMP;
+else
+    EEGTMP = EEG;
+    if time * 6 > EEG.xmax
+        validtime = 0;
+        EEGTMP.vhtp.eeg_htpEegRemoveChansEeglab.completed=0;
+        EEGTMP.vhtp.eeg_htpEegRemoveChansEeglab.failReason = 'Data too short';
+    else
+        cut1 = [0 time];
+        EEGTMP = eeg_checkset(pop_select(EEGTMP, 'notime', cut1));
+        cut2 = [(EEGTMP.xmax - time) EEGTMP.xmax];
+        EEGTMP = eeg_checkset(pop_select(EEGTMP, 'notime', cut2));
     end
+
+    EEG = EEGTMP;
+end
 
 end
 
-function EEG = autobadchannel( EEG, threshold )
+function EEG = autobadchannel( EEG, threshold, automark_true )
+
+if automark_true % added 6/17/22 default turned off (saves time)
 
     maxchannels = floor(size(EEG.data, 1) * 0.10);
 
@@ -266,21 +293,24 @@ function EEG = autobadchannel( EEG, threshold )
     end
     zerochan = find_zeroed_chans( EEG.data ); if ~isempty( zerochan ), indelec{end+1} = zerochan'; end
     badchans = cell2mat(indelec(1:length(indelec)));
-    EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_autobadchannel = unique( badchans, 'stable' );
+else
+    badchans = [];
+end
+EEG.vhtp.eeg_htpEegRemoveChansEeglab.proc_autobadchannel = unique( badchans, 'stable' );
 end
 
- function index = find_zeroed_chans( dat )
-            
-    chans_median = zeros( size( dat, 1 ),1 );
+function index = find_zeroed_chans( dat )
 
-    for i = 1 : size( dat, 1 )
+chans_median = zeros( size( dat, 1 ),1 );
 
-        chans_median(i) = median( dat(i, :));
+for i = 1 : size( dat, 1 )
 
-    end
+    chans_median(i) = median( dat(i, :));
 
-    allchannels = round(chans_median(:),8);
-    index =  find(allchannels == 0);
+end
+
+allchannels = round(chans_median(:),8);
+index =  find(allchannels == 0);
 
 end
 
