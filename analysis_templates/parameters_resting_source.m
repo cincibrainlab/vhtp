e5d8dd212f2c2d2e753f42931e2c99a72d673354
Ernie Pedapati <ernest.pedapati@cchmc.org>
2022-08-16 00:02:03 -0400

    Update parameters_resting_source.m

--- analysis_templates/parameters_resting_source.m
+++ analysis_templates/parameters_resting_source.m
@@ -1,13 +1,22 @@
-function [PARAMS] = parameters_analysis_resting()
+function [PARAMS] = parameters_resting_source()
 clear PARAMS;
 
-%%RESTING SPECTRAL POWER
+%% Wavelet Thresholding
+PARAMS.eeg_htpEegWaveletDenoiseHappe.function = @eeg_htpEegWaveletDenoiseHappe;
+PARAMS.eeg_htpEegWaveletDenoiseHappe.isErp = false;
+
+
+%% RESTING SPECTRAL POWER
 PARAMS.eeg_htpCalcRestPower.function = @eeg_htpCalcRestPower;
 PARAMS.eeg_htpCalcRestPower.useParquet = true;
 PARAMS.eeg_htpCalcRestPower.gpuOn = true;
+% PARAMS.eeg_htpCalcRestPower.outputdir = results_dir;
 
-PARAMS.eeg_htpCalcAacGlobal.function = @eeg_htpCalcAacGlobal;
-PARAMS.eeg_htpCalcAacGlobal.useParquet = true;
-PARAMS.eeg_htpCalcAacGlobal.duration = 60;
+%% GLOBAL COUPLING
+PARAMS.eeg_htpGraphPhaseBcm.function = @eeg_htpCalcAacGlobal;
+PARAMS.eeg_htpGraphPhaseBcm.useParquet = true;
+PARAMS.eeg_htpGraphPhaseBcm.gpuOn = true;
+% PARAMS.eeg_htpGraphPhaseBcm.outputdir = results_dir;
+
 
 end
