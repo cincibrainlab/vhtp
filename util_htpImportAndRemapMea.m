2eb6415ceb5e539e5a77ef03ec2c67456905baa6
NSuer <122294714+NSuer@users.noreply.github.com>
2023-07-13 11:09:36 -0400

    Update util_htpImportAndRemapMea.m
    Adding code to correctly remap channels for Xdat

--- util_htpImportAndRemapMea.m
+++ util_htpImportAndRemapMea.m
@@ -9,8 +9,8 @@ note = @(msg) fprintf('%s: %s\n', mfilename, msg );
 if ~isempty(regexp(dataFile,'xdat','match')); xdatFile = true; else; xdatFile = false; end
 
 if xdatFile
-    try [signalStruct,timeRange,jsonData] = xdatImport(extractBefore(dataFile,'_data'));
-
+    try 
+        [signalStruct,timeRange,jsonData] = xdatImport(extractBefore(dataFile,'_data'));
         stage1_map = readtable("MouseEEGv2H32_Import_Stage1.csv");
         stage2_map = readtable("MouseEEGv2H32_Import_Stage2.csv");
 
@@ -77,8 +77,54 @@ if xdatFile
 %             EEG.chanlocs(i).Z = getZ(stage2_map, EEG.chanlocs(i).labels);
             %disp(EEG.chanlocs(i).labels);
         end
+
+
+        % Dictionary for correct mappings 
+        mappingDict = containers.Map();
+        % Current location : Correct location 
+        mappingDict(1)  = 29;
+        mappingDict(2)  = 27;
+        mappingDict(3)  = 25;
+        mappingDict(4)  = 23;
+        mappingDict(5)  = 21;
+        mappingDict(6)  = 19;
+        mappingDict(7)  = 17;
+        mappingDict(8)  = 30;
+        mappingDict(9)  = 28;
+        mappingDict(10) = 26;
+        mappingDict(11) = 24;
+        mappingDict(12) = 22;
+        mappingDict(13) = 20;
+        mappingDict(14) = 18;
+        mappingDict(15) = 16;
+        mappingDict(16) = 14;
+        mappingDict(17) = 12;
+        mappingDict(18) = 10;
+        mappingDict(19) = 8;
+        mappingDict(20) = 6;
+        mappingDict(21) = 4;
+        mappingDict(22) = 2;
+        mappingDict(23) = 1;
+        mappingDict(24) = 15;
+        mappingDict(25) = 13;
+        mappingDict(26) = 11;
+        mappingDict(27) = 9;
+        mappingDict(28) = 7;
+        mappingDict(29) = 5;
+        mappingDict(30) = 3;
+
+        %Remapping the data to correct locations 
+        % Create a temporary cell array to hold the reordered values
+        reorderedArray = cell(size(EEG.data));
+        
+        % Reorder the values based on the mapping
+        for currentIdx = 1:numel(EEG.data)
+            correctIdx = mappingDict(currentIdx);
+            reorderedArray{correctIdx} = EEG.data{currentIdx};
+        end
         EEG = eeg_checkset(EEG);
         EEG = eeg_checkchanlocs(EEG);
+        EEG.data = reorderedArray;
 
 
     catch
