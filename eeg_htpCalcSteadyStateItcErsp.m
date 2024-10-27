771e152664764fad9a971793b97b7f1a136c74f2
Ernie Pedapati <ernest.pedapati@cchmc.org>
2024-10-27 19:58:47 -0400

    assr scripts

---
+++ eeg_htpCalcSteadyStateItcErsp.m
@@ -0,0 +1,348 @@
+function [EEG, results] = eeg_htpCalcSteadyStateItcErsp(EEG, varargin)
+% Description: calculate ITC and single trial power (ERSP)
+% ShortTitle: Auditory Chirp Analysis
+% Category: Analysis
+% Tags: ERP
+%      Original code designed for auditory chirp presentation.
+%      Note: newtimef() is a legacy EEGLAB function that is highly
+%      dependent on EEGLAB timefreq.m which is frequently updated. Future
+%      version of this code will embed relevant itc/ersp functions.
+%
+% Usage:
+%    >> [ EEG ] = eeg_htpCalcSteadyStateItcErsp( EEG, varargin )
+%
+% Require Inputs:
+%     EEG       - EEGLAB Structure
+%
+% Function Specific Inputs:
+%     'option1' - description
+%
+% Outputs:
+%     EEG       - EEGLAB Structure with modified .etc.htp field
+%     results   - etc.htp results structure or customized
+%     baselinew - if 'baselinew' vector is specified ersp will be
+%     calculated and added to the result spreadsheet. 
+%
+% Example: with baseline ERSP
+%  eeg_htpCalcSteadyStateItcErsp(EEG, 'sourceOn', true, 'byChannel', true, 'baselinew', [-500 0]);
+%
+%  This file is part of the Cincinnati Visual High Throughput Pipeline,
+%  please see http://github.com/cincibrainlab
+%
+%  Contact: kyle.cullion@cchmc.org
+
+timestamp = datestr(now, 'yymmddHHMMSS'); % timestamp
+functionstamp = mfilename; % function name for logging/output
+
+% Inputs: Function Specific
+% publication parameters: Ethridge et al. 2019
+defaultTLimits = [-1000 4000];
+defaultBaselineW = [-1000 0];
+defaultFLimits = [2 110];
+defaultTimesOut = 250;
+defaultCycles = [1 30];
+defaultWinSize = 100;
+defaultNFreqs = 109;
+defaultSourceOn = false;
+defaultEmptyEEG = true;
+defaultAmpThreshold = 200;
+defaultByChannel = false;
+defaultOutputDir = tempdir;
+
+% MATLAB built-in input validation
+ip = inputParser();
+addRequired(ip, 'EEG', @isstruct);
+addParameter(ip, 'tlimits', defaultTLimits, @isvector);
+addParameter(ip, 'flimits', defaultFLimits, @isvector);
+addParameter(ip, 'cycles', defaultCycles, @isvector);
+addParameter(ip, 'timesout', defaultTimesOut, @isinteger);
+addParameter(ip, 'winsize', defaultWinSize, @isinteger);
+addParameter(ip, 'nfreqs', defaultNFreqs, @isinteger);
+addParameter(ip, 'outputdir', defaultOutputDir, @isfolder);
+addParameter(ip, 'sourceOn', defaultSourceOn, @islogical);
+addParameter(ip, 'emptyEEG', defaultEmptyEEG, @islogical)
+addParameter(ip, 'ampThreshold', defaultAmpThreshold, @isnumeric)
+addParameter(ip, 'byChannel', defaultByChannel, @islogical)
+addParameter(ip, 'baselinew', defaultBaselineW, @isvector);
+parse(ip, EEG, varargin{:});
+
+outputdir = ip.Results.outputdir;
+
+% base output file can be modified with strrep()
+outputfile = fullfile(outputdir, [functionstamp '_' EEG.setname '_' timestamp '.mat']);
+
+% START: Signal Processing
+
+% create critical values correction
+for i = 1:400, rcrits(i, 1) = sqrt(- (1 / i) * log(.5)); end
+
+if EEG.trials < 10, error("Low number of trials detected; check epoching."); end
+
+% amplitude based artifact rejection
+amp_threshold = ip.Results.ampThreshold;
+bad_trial_idx = [];
+bad_trial_count = 0;
+
+for i = 1:EEG.trials
+
+    trial_amplitude = abs(mean(EEG.data(:, :, i), 3));
+    trial_index = i;
+
+    if any(any(trial_amplitude > amp_threshold))
+        bad_trial_count = bad_trial_count +1;
+        bad_trial_idx(bad_trial_count) = trial_index;
+        bad_trial_label = sprintf("%s epoch: %d", EEG.setname, trial_index);
+    end
+
+end
+
+if ~isempty(bad_trial_idx)
+    EEG = pop_select(EEG, 'notrial', bad_trial_idx);
+    disp(['Removed: ' EEG.setname ' ' num2str(bad_trial_idx)])
+end
+
+% define ROI of auditory cortex projection
+chirp_electrode_labels = {'E23', 'E18', 'E16', 'E10', 'E3', ...
+    'E28', 'E24', 'E19', 'E11', 'E4', 'E124', 'E117', ...
+    'E29', 'E20', 'E12', 'E5', 'E118', 'E111', 'E13', 'E6', 'E112', ...
+    'E7', 'E106'};
+
+dksource_labels = {EEG.chanlocs.labels};
+chirp_dksource_labels_idx = contains(dksource_labels, 'temporal');
+chirp_dksource_labels = dksource_labels(chirp_dksource_labels_idx);
+
+if ip.Results.sourceOn
+    chirp_sensors = chirp_dksource_labels;
+else
+    if EEG.nbchan < 128, error('Insufficient # of Channels. Check if SourceOn = false.'); end
+    chirp_sensors = chirp_electrode_labels;
+    chan_label = "Frontal";
+end
+
+sensoridx = cell2mat(cellfun(@(x) find(strcmpi(x, {EEG.chanlocs.labels})), ...
+    chirp_sensors, 'uni', 0));
+
+tlimits = ip.Results.tlimits;
+flimits = ip.Results.flimits; % changed from 120 to 110 - LAD 1/19/22
+timesout = ip.Results.timesout;
+cycles = ip.Results.cycles;
+winsize = ip.Results.winsize;
+nfreqs = ip.Results.nfreqs; % changed from 119 to 109 - LAD 1/19/22
+
+Fs = EEG.srate;
+trials = EEG.trials;
+EEG.subject = EEG.setname;
+
+% newtimef() - Return estimates and plots of mean event-related (log) spectral
+% perturbation (ERSP) and inter-trial coherence (ITC) events across
+% event-related trials (epochs) of a single input channel time series.
+
+% define analysis parameters
+if ip.Results.byChannel
+    data = EEG.data;
+else
+    data = mean(EEG.data(sensoridx, :, :));
+end
+
+for ci = 1 : size(data,1)
+    datax = squeeze(data(ci,:,:));
+    if ip.Results.byChannel
+        channame = dksource_labels{ci};
+    else
+        channame = "Average";
+    end
+
+    [stp1, itc, ~, t_s, f_s] = newtimef(double(datax), ...
+        EEG.pnts, ... % frames
+        tlimits, ... %tlimits
+        Fs, ... %Fs
+        cycles, ... % varwin (cycles)
+        'winsize', winsize, ...
+        'nfreqs', nfreqs, ...
+        'freqs', flimits, ...
+        'plotersp', 'off', ...
+        'plotitc', 'off', ...
+        'verbose', 'off', ...
+        'baseline', NaN, ...
+        'timesout', timesout);
+
+    % Single trial power with baseline correction (ERSP)
+    if ~isnan(ip.Results.baselinew)
+        [ersp1, itc, ~, t_s, f_s] = newtimef(double(datax), ...
+        EEG.pnts, ... % frames
+        tlimits, ... %tlimits
+        Fs, ... %Fs
+        cycles, ... % varwin (cycles)
+        'winsize', winsize, ...
+        'nfreqs', nfreqs, ...
+        'freqs', flimits, ...
+        'plotersp', 'off', ...
+        'plotitc', 'off', ...
+        'verbose', 'off', ...
+        'baseline', ip.Results.baselinew, ...
+        'timesout', timesout);
+    else
+        ersp1 = nan(size(stp1));
+    end
+
+    corrected_itc = (abs(itc)) - rcrits(trials);
+    uncorrected_itc = (abs(itc));
+
+    % results table
+    findHz = @(hz1, hz2) find(f_s >= hz1 & f_s <= hz2);
+    findTimes = @(t1, t2) find(t_s >= t1 & t_s <= t2);
+
+    %% Identify ROIs: Units are Hz for Frequencies and milliseconds for Times
+    roi.ERSP_gamma_hz = {findHz(30, 71)}; % edited to LE's final analyzed frequences for gamma - LAD 1/19/22
+    roi.ERSP_gamma1_hz = {findHz(30, 60)};
+    roi.ERSP_gamma2_hz = {findHz(60, 100)};
+    roi.ERSP_alpha_hz = {findHz(8, 12)};
+
+    roi.ItcOnset_hz = {findHz(2, 13)}; % LE's final freq range for onset was 6-13hz - should check with her about this
+    roi.ItcOnset_ms = {findTimes(92, 308)};
+    roi.ItcOffset_hz = {findHz(2, 13)};
+    roi.ItcOffset_ms = {findTimes(2038, 2254)};
+
+    roi.ITC40_hz_og = {findHz(31, 42); findHz(43, 46); findHz(47, 57)};
+    roi.ITC40_ms_og = {findTimes(676, 785); findTimes(796, 981); findTimes(988, 1066)};
+
+    %roi.ITC40_hz = {findHz(30, 35); findHz(35, 40); findHz(40, 45); findHz(45, 50); findHz(50, 55)};
+    %roi.ITC40_ms = {findTimes(650, 850); findTimes(750, 950); findTimes(850, 1050); ...
+    %    findTimes(950, 1150); findTimes(1050, 1250)};
+
+    roi.ITC40_hz = {findHz(35, 45)};
+    roi.ITC40_ms = {findTimes(0, 3000)};
+
+    %roi.ITC80_hz = {find(f_s >= 70 & f_s <= 100)};
+    %roi.ITC80_ms = {find(t_s >= 1390 & t_s <= 1930)};
+
+    roi.ITC80_hz = {findHz(75, 85)};
+    roi.ITC80_ms = {findTimes(0, 3000)};
+
+        % Summary Functions
+    computeMeanSTP = @(roi_hz) sum(cellfun(@(hz) mean2(stp1(hz, :)), ...
+        roi_hz)) / numel(roi_hz);
+
+    computeMeanERSP = @(roi_hz) sum(cellfun(@(hz) mean2(ersp1(hz, :)), ...
+        roi_hz)) / numel(roi_hz);
+
+    computeMeanITC = @(roi_hz, roi_ms) sum(cellfun(@(hz, ms) mean2(corrected_itc(hz, ms)), ...
+        roi_hz, roi_ms)) / numel(roi_hz);
+
+    computeMeanRawITC = @(roi_hz, roi_ms) sum(cellfun(@(hz, ms) mean2(uncorrected_itc(hz, ms)), ...
+        roi_hz, roi_ms)) / numel(roi_hz);
+
+    plotroi = true;
+
+    if plotroi
+
+        [~,basename,~] = fileparts(EEG.filename);
+        basename = char(sprintf("%s_%s", basename, channame));
+        
+        % Plot the uncorrected ITC using imagesc
+        fig = figure('Visible','off');
+        itc = corrected_itc;
+        imagesc(t_s, f_s, itc);
+        axis xy;  % Ensures that the Y-axis is not flipped
+        xlabel('Time (ms)');
+        ylabel('Frequency (Hz)');
+        title(sprintf('ITC Heatmap %s %2.4f (max: %2.4f)', basename, computeMeanITC(roi.ITC40_hz, roi.ITC40_ms), max(max(itc))));
+        colorbar;
+
+        % Define the ROI for ITC40
+        hz_indices = roi.ITC40_hz{1};  % Indices of frequencies for ITC40 (e.g., 39-41 Hz)
+        ms_indices = roi.ITC40_ms{1};  % Indices of time for ITC40 (e.g., 600-2400 ms)
+
+        % Get the frequency and time limits for the box
+        f_min = f_s(hz_indices(1));  % Minimum frequency in the ROI
+        f_max = f_s(hz_indices(end)); % Maximum frequency in the ROI
+        t_min = t_s(ms_indices(1));   % Minimum time in the ROI
+        t_max = t_s(ms_indices(end));  % Maximum time in the ROI
+
+        max_itc = .25;
+        if max(max(itc)) > max_itc
+            % axis([0 1]); % important
+            % plot_title = [plot_title ' (EXCEEDS UPPER LIMIT ' num2str(max_itc) ')' ];
+            caxis([0 .25]); % important
+
+        else
+            %caxis([0 .25]); % important
+            caxis([0 .25]); % important
+
+        end
+
+        % Draw a rectangle around the ROI
+        hold on;  % Keep the current plot
+        rectangle('Position', [t_min, f_min, t_max - t_min, f_max - f_min], ...
+            'EdgeColor', 'r', 'LineWidth', 2);  % Red box with a line width of 2
+        hold off;
+
+        saveas(fig, fullfile(outputdir, [basename '.png']))
+        close(fig);  % Close the figure window
+    
+
+    end
+    
+    csvRow = { ...
+        EEG.filename ...
+        EEG.trials ...
+        channame ...
+        numel(bad_trial_idx) ...
+        computeMeanSTP(roi.ERSP_gamma_hz) ...
+        computeMeanSTP(roi.ERSP_gamma1_hz) ...
+        computeMeanSTP(roi.ERSP_gamma2_hz) ...
+        computeMeanSTP(roi.ERSP_alpha_hz) ...
+        computeMeanERSP(roi.ERSP_gamma_hz) ...
+        computeMeanERSP(roi.ERSP_gamma1_hz) ...
+        computeMeanERSP(roi.ERSP_gamma2_hz) ...
+        computeMeanERSP(roi.ERSP_alpha_hz) ...
+        computeMeanITC(roi.ITC40_hz_og, roi.ITC40_ms_og) ...
+        computeMeanITC(roi.ITC40_hz, roi.ITC40_ms) ...
+        computeMeanITC(roi.ITC80_hz, roi.ITC80_ms) ...
+        computeMeanITC(roi.ItcOnset_hz, roi.ItcOnset_ms) ...
+        computeMeanITC(roi.ItcOffset_hz, roi.ItcOffset_ms) ...
+        computeMeanRawITC(roi.ITC40_hz_og, roi.ITC40_ms_og) ...
+        computeMeanRawITC(roi.ITC40_hz, roi.ITC40_ms) ...
+        computeMeanRawITC(roi.ITC80_hz, roi.ITC80_ms) ...
+        computeMeanRawITC(roi.ItcOnset_hz, roi.ItcOnset_ms) ...
+        computeMeanRawITC(roi.ItcOffset_hz, roi.ItcOffset_ms) ...
+        };
+
+    chanCsvRows{ci} = csvRow;
+    chanItc{ci} = corrected_itc;
+    chanRawItc{ci} = uncorrected_itc;
+    chanErsp{ci} = ersp1;
+    chanStp{ci} = stp1;
+    disp(channame);
+end
+
+csvRow = vertcat(chanCsvRows{:});
+
+if ip.Results.emptyEEG, EEG.data = []; end
+
+csvTable = cell2table(csvRow, "VariableNames", {'eegid', 'trials', 'chan', 'rejtrials', ...
+    'stp_gamma', 'stp_gamma1', 'stp_gamma2', 'stp_alpha', ...
+    'ersp_gamma', 'ersp_gamma1', 'ersp_gamma2', 'ersp_alpha', ...
+    'itc40_og', 'itc40', 'itc80', 'itconset', 'itcoffset', ...
+    'raw_itc40_og', 'raw_itc40', 'raw_itc80', 'raw_itconset', 'raw_itcoffset',});
+
+% END: Signal Processing
+
+% QI Table
+qi_table = cell2table({EEG.filename, functionstamp, timestamp}, 'VariableNames', {'eegid', 'scriptname', 'timestamp'});
+
+% Outputs:
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.itc1 = cat(3,chanItc{:});
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.rawitc1 = cat(3,chanRawItc{:});
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.ersp1 = cat(3,chanErsp{:});
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.stp1 = cat(3,chanStp{:});
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.t_s = t_s;
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.f_s = f_s;
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.summary_table = csvTable;
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.qi_table = qi_table;
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.trials = EEG.trials;
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.amp_rej_trials = num2str(bad_trial_idx);
+EEG.vhtp.eeg_htpCalcSteadyStateItcErsp.amp_threshold = amp_threshold;
+results =  EEG.vhtp.eeg_htpCalcSteadyStateItcErsp;
+
+end
---
+++ eeg_htpVisualizeSteadyStateItcErsp.m
@@ -0,0 +1,345 @@
+function [EEGcell, results] = eeg_htpVisualizeSteadyStateItcErsp( EEGcell, varargin )
+% Description: Creates ERP plots from eeg_htpCalcSteadyStateItcErsp.
+% ShortTitle: Visualize assr ERP analysis
+% Category: Analysis
+% Tags: ERP
+%
+% Usage:
+%    >> [ EEGcell, results ] = eeg_htpVisualizeSteadyStateItcErsp( EEGcell )
+%
+% Require Inputs:
+%     EEGcell       - cell array of EEGLAB Structures
+% Function Specific Inputs:
+%     'outputdir' - output directory for save files
+%     'groupIds'  - vector length of EEGcell with integers representing
+%                   groups
+%     'groupmean' - (true/false) average across groups
+%     'singleplot'- (true/false) One plot for group and individual (multiline)
+%     'contrasts' - cell array of contrast pairs with group indexes {{1,2}}
+%     = group 1- group 2
+%     'averageByRegion' - for source analysis if region_chanlocs,
+%     region_itc1 and region_ersp1 is present it will created images
+%     based on mean region. Code for DK atlas is present, but can be
+%     modified.
+%
+% Outputs:
+%     EEGcell       - cell array of EEGLAB Structure with modified .etc.htp field
+%     results   - etc.htp results structure or customized
+%
+%  This file is part of the Cincinnati Visual High Throughput Pipeline,
+%  please see http://github.com/cincibrainlab
+%
+%  Contact: kyle.cullion@cchmc.org
+
+timestamp    = datestr(now,'yymmddHHMMSS');  % timestamp
+functionstamp = mfilename; % function name for logging/output
+
+% Inputs: Function Specific
+
+% Inputs: Common across Visual HTP functions
+defaultOutputDir = tempdir;
+defaultGroupIds = ones(1,length(EEGcell));
+defaultGroupMean = 1;
+defaultSingleplot = 1;
+defaultAverageByRegion = false;
+defaultContrasts = {};
+defaultDiffColorLimits = [];
+defaultChannel = missing;
+EEGno =  numel(EEGcell);
+
+% MATLAB built-in input validation
+ip = inputParser();
+errorMsg = 'Group Id vector must be same length as input';
+validGroupIds = @(x) assert(numel(x) == EEGno, errorMsg);
+
+errorMsg2 = 'EEG input should be either a cell array or struct.';
+validEegArray = @(x) assert(iscell(x) || isstruct(x), errorMsg2);
+
+addRequired(ip, 'EEGcell', validEegArray);
+addParameter(ip,'outputdir', defaultOutputDir, @isfolder);
+addParameter(ip,'groupids', defaultGroupIds, validGroupIds);
+addParameter(ip,'groupmean', defaultGroupMean, @islogical);
+addParameter(ip,'singleplot', defaultSingleplot, @islogical);
+addParameter(ip, 'averageByRegion', defaultAverageByRegion, @islogical);
+addParameter(ip,'contrasts', defaultContrasts, @iscell);
+addParameter(ip,'diffColorLimits', defaultDiffColorLimits, @isvector);
+addParameter(ip,'channel', defaultChannel, @ischar); % specific channel for plotting
+
+parse(ip,EEGcell,varargin{:});
+outputdir = ip.Results.outputdir;
+isDiff = false;
+
+if isstruct(EEGcell)
+    warning('Struct passed, converting to Cell.')
+    EEGcell = num2cell(EEGcell);
+end
+
+% base output file can be modified with strrep()
+outfileCell = cellfun( @(EEG) fullfile(outputdir, ...
+    [functionstamp '_'  EEG.setname '_' timestamp '.mat']), ...
+    EEGcell, 'uni',0);
+
+% if averageByRegions is present will reduce dimensions of channel data by
+% averaging into regions.
+if ip.Results.averageByRegion
+    for ei = 1 : numel(EEGcell)
+        EEGcell{ei} = eeg_htpAverageStructByRegion( EEGcell{ei}, 'scriptname','eeg_htpVisualizeSteadyStateItcErsp');
+    end
+end
+
+% % filter for specific channel if needed
+% chanOfInterest = ip.Results.channel;
+% if ~ismissing(chanOfInterest)
+% fprintf("Single Channel Mode: %s\n", chanOfInterest);
+% if any(strcmp(chanOfInterest, {EEGcell{1}.chanlocs.labels}))
+%     for i = 1 : numel(EEGcell)
+%         chanIdx = find(strcmp(chanOfInterest, {EEGcell{i}.chanlocs.labels}));
+%         EEGcell{i}.chanlocs = EEGcell{i}.chanlocs{chanIdx};
+%     end
+% else
+%     fprintf("%s channel not found, check input.\n", chanOfInterest);
+% end
+% end
+
+% START: Start Visualization
+chanItc = {};
+chanErsp = {};
+chanStp = {};
+% check for multichannel data
+if ndims(EEGcell{1}.vhtp.eeg_htpCalcSteadyStateItcErsp.itc1) > 2
+    isMultiChannel = true;
+    chanlabels = {EEGcell{1}.chanlocs.labels};
+    for ei = 1 : numel(EEGcell)
+        chanItc{ei}  = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.itc1;
+        chanErsp{ei} = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.ersp1;
+        chanStp{ei}  = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.stp1;
+    end
+else
+    isMultiChannel = false;
+    chanlabels = {'average'};
+end
+
+for ci = 1 : numel(chanlabels)
+    channame = chanlabels{ci};
+    if isMultiChannel
+        for ei = 1 : length(EEGcell)
+            chanItcNow = chanItc{ei};
+            chanErspNow = chanErsp{ei};
+            chanStpNow = chanStp{ei};
+            EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.itc1 = chanItcNow(:,:,ci);
+            EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.ersp1 = chanErspNow(:,:,ci);
+            EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.stp1 = chanStpNow(:,:,ci);
+        end
+    end
+
+    % get groups
+    if iscell(ip.Results.groupids) || iscategorical(ip.Results.groupids)
+        groups = unique(categorical(ip.Results.groupids));
+        group_list = ip.Results.groupids;
+    else
+        groups = double(unique(ip.Results.groupids));
+        group_list = categorical(ip.Results.groupids);
+    end
+    group_no = numel(unique(groups));
+
+    % consistent indexes regardless of group or inidividual ERP
+    t = EEGcell{1}.vhtp.eeg_htpCalcSteadyStateItcErsp.t_s;
+    f = EEGcell{1}.vhtp.eeg_htpCalcSteadyStateItcErsp.f_s;
+
+    % calculate ERP mean, individual ERPs, or single plot ERPs
+    plot_title = [];
+    if ip.Results.groupmean  % single mean across groups
+        plot_title = ['PLACEHOLDER by Group (' channame ')'];
+        for ei = 1 : length(EEGcell) % all ERPs in single array
+            itcArr(:,:,ei) = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.itc1;
+            erspArr(:,:,ei) = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.ersp1;
+            stpArr(:,:,ei) = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.stp1;
+        end
+
+        for gi = 1 : group_no % mean each by group id
+            cur_group_idx = find(ip.Results.groupids == groups(gi));
+            group_names{gi} =  num2str(groups(gi));
+            plot_title_cell{gi} = sprintf('PLACEHOLDER for group %s (%s)',  char(groups(gi)), channame);
+            plot_filename_cell{gi} = fullfile(outputdir, ...
+                ['assr_itcersp_by_group_' channame '_' num2str(groups(gi)) '_' timestamp '.png']);
+            itc(:,:,gi) = mean(itcArr(:,:,cur_group_idx),3, "omitnan");
+            ersp(:,:,gi) = mean(erspArr(:,:,cur_group_idx),3, "omitnan");
+            stp(:,:,gi) = mean(stpArr(:,:,cur_group_idx),3, "omitnan");
+
+        end
+        % Perform contrasts and add as additional groups
+        get_id = @(x) find(strcmp(group_names, x));
+
+        if ~isempty(ip.Results.contrasts)
+            contrasts = ip.Results.contrasts;
+            isDiff = true;
+            for contrast_i = 1 : numel(contrasts)
+                select_contrast = contrasts{contrast_i};
+                group_no = group_no + 1;
+
+                %id1 = get_id(select_contrast(1));
+                %id2 = get_id(select_contrast(2));
+
+                id1 = select_contrast{1};
+                id2 = select_contrast{2};
+
+                itc(:,:,group_no) = itc(:,:,id1) - itc(:,:,id2);
+                ersp(:,:,group_no) = ersp(:,:,id1) - ersp(:,:,id2);
+                stp(:,:,group_no) = stp(:,:,id1) - stp(:,:,id2);
+
+                plot_title_cell{group_no} = sprintf('PLACEHOLDER for group diff\n%s_%s (%s)',num2str(select_contrast{1}), num2str(select_contrast{2}), channame);
+                plot_filename_cell{group_no} = fullfile(outputdir, ...
+                    ['assr_itcersp_by_groupdiff_' channame '_' sprintf('%s_%s', num2str(select_contrast{1}), num2str(select_contrast{2})) '_' timestamp '.png']);
+            end
+        else
+            ifDiff = false;
+        end
+
+    else  % individual results
+        for ei = 1 : length(EEGcell)
+            if ip.Results.singleplot && ei == 1 && ~ip.Results.groupmean
+                plot_title = 'PLACEHOLDER by Recording';
+                plot_filename = fullfile(outputdir,['assr_itcersp_by_recording_' channame '_' timestamp '.png']);
+                [~, temp_plot_name, ~] = fileparts(EEGcell{ei}.filename);
+                plot_title_cell{ei} = sprintf('PLACEHOLDER for %s (%s)', temp_plot_name, channame);
+                plot_filename_cell{ei} = fullfile(outputdir, ...
+                    [temp_plot_name 'assr_itc' channame '.png']);
+
+            else
+                [~, temp_plot_name, ~] = fileparts(EEGcell{ei}.filename);
+                plot_title_cell{ei} = sprintf('PLACEHOLDER for %s (%s)', temp_plot_name, channame);
+                plot_filename_cell{ei} = fullfile(outputdir, ...
+                    [temp_plot_name 'assr_itc' channame '.png']);
+            end
+            itc(:,:,ei) = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.itc1;
+            ersp(:,:,ei) = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.ersp1;
+            stp(:,:,ei) = EEGcell{ei}.vhtp.eeg_htpCalcSteadyStateItcErsp.stp1;
+        end
+        
+    end
+
+    if ip.Results.singleplot
+        for gi = 1 : group_no
+            figure('Position', [600 600 1200 700]);
+            subplot(1,3,1)
+            createPlot_assrItc(t, f, itc(:,:,gi), ...
+                strrep(plot_title_cell{gi},'PLACEHOLDER','ITC'));
+            subplot(1,3,2)
+            createPlot_assrStp(t, f, stp(:,:,gi), ...
+                strrep(plot_title_cell{gi},'PLACEHOLDER','STP'));
+            subplot(1,3,3)
+            createPlot_assrErsp(t, f, ersp(:,:,gi), ...
+                strrep(plot_title_cell{gi},'PLACEHOLDER','ERSP'));
+            sgtitle(strrep(plot_title_cell{gi},'PLACEHOLDER','assr Plots'));
+            saveas(gcf, plot_filename_cell{gi});
+            close all;
+        end
+    else
+        for si = 1 : size(itc,3)
+            disp(si);
+            figure('Position', [600 600 1200 700], 'Visible', 'off');
+            subplot(1,3,1)
+            if isDiff && si > numel(unique(groups))
+                addtitle = plot_title_cell{si};
+            else
+                addtitle = '';
+            end
+            createPlot_assrItc(t, f, itc(:,:,si), ...
+                ['ITC ' addtitle]);
+            subplot(1,3,2)
+            createPlot_assrStp(t, f, stp(:,:,si), ...
+                ['STP ' addtitle]);
+            subplot(1,3,3)
+            createPlot_assrErsp(t, f, ersp(:,:,si), ...
+                ['ERSP ' addtitle]);
+            sgtitle(strrep(plot_title_cell{si},'PLACEHOLDER','assr Plots'));
+            saveas(gcf, plot_filename_cell{si});
+            close all;
+        end
+    end
+end
+
+% END: End Visualization
+
+% QI Table
+qi_table = cellfun( @(EEG) ...
+    cell2table({EEG.setname, functionstamp, timestamp}, ...
+    'VariableNames', {'eegid','scriptname','timestamp'}), ...
+    EEGcell, 'uni',0);
+
+% Outputs:
+results = [];
+end
+
+function fig = createPlot_assrItc(t, f, itc, plot_title)
+
+set(0,'defaultTextInterpreter','none');
+colormap jet;
+imagesc(t,f,itc); axis xy;
+xlabel('Time (ms)'); ylabel('Frequency (Hz)');
+h = colorbar;
+ylabel(h,'Intertrial Coherence (ITC)');
+%ylim(h,[0 .2]);
+if contains(plot_title, 'diff')
+        caxis([-.1 .1]); % important
+else
+    max_itc = .2;
+    if max(max(itc)) > max_itc
+        % axis([0 1]); % important
+        plot_title = [plot_title ' (EXCEEDS UPPER LIMIT ' num2str(max_itc) ')' ];
+    else
+        caxis([0 .1]); % important
+    end
+end
+pbaspect([1 1 1]);
+title(plot_title);
+
+end
+
+function fig = createPlot_assrStp(t, f, stp,plot_title)
+
+set(0,'defaultTextInterpreter','none');
+colormap jet;
+imagesc(t,f,stp); axis xy;
+xlabel('Time (ms)'); ylabel('Frequency (Hz)');
+h = colorbar;
+ylabel(h,'Power (dB/Hz) ');
+%ylim(h,[0 45]);
+if contains(plot_title, 'diff')
+    caxis([-3 3]); % important
+else
+    max_stp = -190;
+    if max(max(stp)) > max_stp
+        caxis([0 50]); % important
+        plot_title = [plot_title ' (EXCEEDS UPPER LIMIT ' num2str(max_stp) ')' ];
+    else
+        caxis([-215 -190]); % important
+    end
+ %   caxis([-215 -190]);
+end
+
+pbaspect([1 1 1]);
+title(plot_title);
+
+end
+
+function fig = createPlot_assrErsp(t, f, ersp,plot_title)
+
+set(0,'defaultTextInterpreter','none');
+colormap jet;
+imagesc(t,f,ersp); axis xy;
+xlabel('Time (ms)'); ylabel('Frequency (Hz)');
+h = colorbar;
+ylabel(h,'Power (dB/Hz) Change from Baseline');
+%ylim(h,[0 45]);
+if contains(plot_title, 'diff')
+    caxis([-1 1]); % important
+else
+    caxis([-2 2]); % important
+%    caxis([-215 -190]);
+end
+
+pbaspect([1 1 1]);
+title(plot_title);
+
+end
