4da61812c130f297f6dce60ae1857a958472f83a
Ernie Pedapati <ernest.pedapati@cchmc.org>
2024-11-22 10:36:43 -0500

    added conditional around gpu access

--- eeg_htpGraphPhaseBcm.m
+++ eeg_htpGraphPhaseBcm.m
@@ -351,7 +351,10 @@ EEG.vhtp.eeg_htpGraphPhaseBcm.graph_table =  graph_table;
 EEG.vhtp.eeg_htpGraphPhaseBcm.qi_table = qi_table;
 results = EEG.vhtp.eeg_htpGraphPhaseBcm;
 
-reset(gpuDevice);
+if ip.Results.gpuon
+    note('Reseting GPU');
+    reset(gpuDevice);
+end
 
     function [bcm, G]  = create_bcm( chan1_list, chan2_list, weights, chan_labels )
         % create brain connectivity matrix using matlab graph objects
